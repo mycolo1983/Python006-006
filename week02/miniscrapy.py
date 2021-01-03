f3fb49e359b2e7d93737ccdb15a0c6a89dc16f52
mycolo1983 <13668993@qq.com>
2021-01-03 23:33:16 +0800

    Update miniscrapy.py

--- week02/miniscrapy.py
+++ week02/miniscrapy.py
@@ -1,18 +1,18 @@
-import requests
-from lxml import etree
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'}
-r = requests.get('https://www.zhihu.com/question/29978072',headers=headers)
-s = etree.HTML(r.text)
-# 获取问题内容
-q_content = s.xpath('//*[@class="QuestionHeader-title"]/text()')[0]
-# 获取问题描述
-q_describe = s.xpath('//*[@class="RichText ztext"]/text()')[0]
-# 获取关注数和浏览量，这两个属性一样
-q_number = s.xpath('//*[@class="NumberBoard-itemValue"]/text()')
-concern_num = q_number[0]
-browing_num = q_number[1]
-# 打印
-print('问题:',q_content,'\n','描述:',q_describe,'\n','关注数:',concern_num,'\n','浏览量:',browing_num)
+# import requests
+# from lxml import etree
+# headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'}
+# r = requests.get('https://www.zhihu.com/question/29978072',headers=headers)
+# s = etree.HTML(r.text)
+# # 获取问题内容
+# q_content = s.xpath('//*[@class="QuestionHeader-title"]/text()')[0]
+# # 获取问题描述
+# q_describe = s.xpath('//*[@class="RichText ztext"]/text()')[0]
+# # 获取关注数和浏览量，这两个属性一样
+# q_number = s.xpath('//*[@class="NumberBoard-itemValue"]/text()')
+# concern_num = q_number[0]
+# browing_num = q_number[1]
+# # 打印
+# print('问题:',q_content,'\n','描述:',q_describe,'\n','关注数:',concern_num,'\n','浏览量:',browing_num)
 
 
 #! /usr/bin/env python3
